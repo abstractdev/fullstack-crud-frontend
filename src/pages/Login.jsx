3880a1ab2000de89e4814db3b192df87d8f98f21
Andrew R <abstractdev0@gmail.com>
2022-09-27 01:45:32 -0400

    feat: Create Login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import AppLayout from "../components/AppLayout";
+import LoginForm from "../components/LoginForm";
+
+function Login() {
+  return (
+    <AppLayout>
+      <LoginForm />
+    </AppLayout>
+  );
+}
+
+export default Login;
